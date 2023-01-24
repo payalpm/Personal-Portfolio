400a292512e191c722e9a7b0eafd3f37d0519996
payalpm <payalmanghnani11@gmail.com>
2023-01-24 16:28:00 +0530

    second commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,12 @@
+export const Blogs = [
+    {
+        id:1,
+        name:"5 Mistakes to Avoid When Starting Out with JavaScript",
+        tags:["js","beginers",],
+        date:"27 Dec, 2022",
+        
+        link:"https://payalpmtech.hashnode.dev/5-mistakes-to-avoid-when-starting-out-with-javascript"
+    },
+    
+    
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,24 @@
+export const Work = [
+    {
+        id:1,
+        name:"Full-Responsive-Portfolio",
+        description:"It is build on top of the React JS.",
+        tags:["html","css","js"],
+        
+        
+        github:"https://github.com/payalpm/Full-Responsive-React-Portfolio"
+    },
+    {
+        id:2,
+        name:"Twitter-Fake-Account-Detection",
+        description:"Machine learning-based methods were used to detect fake accounts that could mislead people.",
+        tags:["python","ml"],
+        
+       
+        github:"https://github.com/payalpm/Twitter-FakeAccountDetection-main"
+        
+    },
+    
+    
+  
+] 
